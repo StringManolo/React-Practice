b00f8fad17c0988b11e6bdffe1edc0f69e80e22e
stringmanolo <manuelvarelacaldas@gmail.com>
2021-05-06 20:58:05 +0200

    More css

--- social-network/src/components/forgotPassword/ForgotPassword.js
+++ social-network/src/components/forgotPassword/ForgotPassword.js
@@ -1,15 +1,13 @@
 import { useState } from "react";
 
 const ForgotPassword = () => {
-  const [ emailValue, setEmail ] = useState("example@gmail.com");
-  const [ passwordValue, setPassword ] = useState("123");
+  const [ emailValue, setEmail ] = useState("");
 
   const hSubmit = e => {
     e.preventDefault();
 
     let formData = new FormData();
     formData.append("email", emailValue);
-    formData.append("password", passwordValue)
 
     alert("Send email?");
     fetch("http://localhost:8000/forgotPassword", {
@@ -33,21 +31,12 @@ const ForgotPassword = () => {
     setEmail(e.target.value);
   }
 
-  const hPasswordChange = e => {
-    setPassword(e.target.value);
-  }
-
   return (
     <>
-      <h1 className="forgotPasswordTitle">Forgot Password</h1>
+      <h1 className="forgotPasswordTitle">Recover</h1>
       <form onSubmit={hSubmit} className="form formforgotPassword">
         <label className="forgotPasswordLabel">
-          Email:&nbsp;
-          <input type="email" className="forgotPasswordEmail" value={emailValue} onChange={hEmailChange} />
-        </label>
-        <label className="forgotPasswordLabel">
-          Password:&nbsp;
-          <input type="password" className="forgotPasswordPassword" value={passwordValue} onChange={hPasswordChange} />
+          <input type="email" className="forgotPasswordEmail" placeholder="              email:" value={emailValue} onChange={hEmailChange} />
         </label>
         <input type="submit" className="forgotPasswordSubmit" value="submit" />
       </form>
